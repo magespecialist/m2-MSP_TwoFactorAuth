62314572626e01895381e5d868561a77979170c4
Riccardo Tempesta <info@riccardotempesta.com>
2017-07-26 19:47:45 +0200

    WIP

---
+++ Model/Config/Source/Provider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MageSpecialist
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@magespecialist.it so we can send you a copy immediately.
+ *
+ * @category   MSP
+ * @package    MSP_NoSpam
+ * @copyright  Copyright (c) 2017 Skeeller srl (http://www.magespecialist.it)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace MSP\TwoFactorAuth\Model\Config\Source;
+
+class Provider implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'google', 'label' => __('Google Authenticator')],
+            ['value' => 'duo', 'label' => __('Duo Security')],
+        ];
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $options = $this->toOptionArray();
+        $return = [];
+
+        foreach ($options as $option) {
+            $return[$option['value']] = $option['label'];
+        }
+
+        return $return;
+    }
+}
